f3a57f86fa73dd64ee4eedb9fc2d193cdb9f8ee5
Lmex89 <bjt.mex@gmail.com>
2021-02-08 19:19:24 -0600

    color-y-datos

--- src/wheater.js
+++ src/wheater.js
@@ -1,9 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, {useState, useEffect} from 'react';
+import './App.css';
+
+const toCelsius = (F) => {
+  return Math.floor(F - 273);
+};
 
 const Weatercomponent = () => {
   const [coords, setCoords] = useState();
   const [weather, setWeather] = useState();
-    console.log(coords);
+  console.log(coords);
   const getCoordenates = () => {
     const coords = {
       lat: '',
@@ -33,24 +39,29 @@ const Weatercomponent = () => {
   useEffect(() => {
     getCoordenates();
   }, []);
-  
-  if (weather){
-      console.log(weather.coord.lat);
-      return (
-        <div className="Hola">
-          <h1>Wheater Channel</h1>
-          <div className=""> 
-            <p>Latitude: {weather.coord.lat}</p>
-            <p>Longitude: {weather.coord.lon}</p>
-            <p>Pressure : {weather.main.pressure}</p>
-          </div>
+
+  if (weather) {
+    console.log(weather.coord.lat);
+    return (
+      <div className="card col-12 col-sm-8 col-md-6 d-flex justify-content-center">
+        <div className="card-body">
+          <h2>Weather Channel</h2>
+          <p>Latitude: {weather.coord.lat}</p>
+          <p>Longitude: {weather.coord.lon}</p>
+          <p>City : {weather.name}</p>
+          <p>Country : {weather.sys.country}</p>
+          <p>Description : {weather.weather[0].description}</p>
+          <p>Pressure : {weather.main.pressure}</p>
+          <p>Humidity : {weather.main.humidity}</p>
+          <p>Temp : {toCelsius(weather.main.temp)} °C</p>
         </div>
-      );
+      </div>
+    );
   }
   return (
     <div className="Hola">
       <h1>Wheater Channel</h1>
-      <div className=""> 
+      <div className="">
         <p>Latitude: {}</p>
         <p>Longitude: {}</p>
         <p>Pressure : {}</p>
